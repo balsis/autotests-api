60c709b35212849683c7e9f999978ba41848a4ab
artur.balsis <artur.balsis@gmail.com>
2025-06-01 17:53:51 +0300

    Практикуемся в реализации API клиентов. Практика реализации API клиентов: /api/v1/exercises

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -49,7 +49,7 @@ class ExercisesClient(APIClient):
         :param query: Словарь с courseId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(url = "/api/v1/exercises", params = query)
+        return self.get(url="/api/v1/exercises", params=query)
 
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
@@ -58,7 +58,7 @@ class ExercisesClient(APIClient):
         :param exercise_id: Идентификатор задания.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(url = f"/api/v1/exercises/{exercise_id}")
+        return self.get(url=f"/api/v1/exercises/{exercise_id}")
 
     def create_exercise_api(self, request: CreateExerciseRequestDict) -> Response:
         """
@@ -67,7 +67,7 @@ class ExercisesClient(APIClient):
         :param request: Словарь title, courseId, maxScore, minScore, orderIndex, description, estimatedTime
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post(url = "/api/v1/exercises", json = request)
+        return self.post(url="/api/v1/exercises", json=request)
 
     def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestDict) -> Response:
         """
@@ -77,7 +77,7 @@ class ExercisesClient(APIClient):
         :param request: Словарь с title, maxScore, minScore, orderIndex, description, estimatedTime
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(url = f"/api/v1/exercises/{exercise_id}", json = request)
+        return self.patch(url=f"/api/v1/exercises/{exercise_id}", json=request)
 
     def delete_exercise_api(self, exercise_id: str) -> Response:
         """
