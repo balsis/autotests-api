1c69b59da6a7fb2df30c8a01a5288e0da35e8f91
artur.balsis <artur.balsis@gmail.com>
2025-06-12 14:23:28 +0300

    Знакомство с Faker. Создаем генератор случайных данных на основе Faker

--- httpx_update_user.py
+++ httpx_update_user.py
@@ -1,10 +1,10 @@
 import httpx
 
-from tools.fakers import get_random_email
+from tools.fakers import fake
 
 
 create_user_payload = {
-    "email": get_random_email(),
+    "email": fake.email(),
     "password": "string",
     "lastName": "string",
     "firstName": "string",
@@ -31,7 +31,7 @@ user_id = create_user_response_data["user"]["id"]
 update_user_headers = {"Authorization": f"Bearer {access_token}"}
 
 update_user_payload = {
-    "email": get_random_email(),
+    "email": fake.email(),
     "lastName": "string",
     "firstName": "string",
     "middleName": "string"
